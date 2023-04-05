bf2d1a84dfce0bb694f22e77ad7e6bc785f64305
foghill <ben@thefoghill.com>
2023-04-05 14:27:27 -0400

    controlled form

--- phasetwojson/src/CharacterForm.js
+++ phasetwojson/src/CharacterForm.js
@@ -2,34 +2,32 @@ import React, { useState } from "react";
 
 function CharacterForm({ handleAddCharacter }) {
   const [name, setName] = useState("");
- 
+  const [status, setStatus] = useState("Alive");
+  const [species, setSpecies] = useState("Human");
+
+  // Adding state hooks for status and species to store the selected values.
+// Changing the onSubmit function to use the selected status and species values.
+// Adding select elements for the status and species fields, with their values controlled by their respective state hooks.
+// Updating the setName hook to clear the input field.
+// Adding setStatus and setSpecies hooks to clear the select fields.
+
   function onSubmit(e) {
     e.preventDefault();
-  
-    // Define arrays of possible status and species categories
-    const statuses = ["Alive", "Dead", "Unknown"];
-    const species = ["Human", "Alien", "Robot"];
-  
-    // Generate a random index for each array
-    const randomStatusIndex = Math.floor(Math.random() * statuses.length);
-    const randomSpeciesIndex = Math.floor(Math.random() * species.length);
   
     const newCharacter = {
       name,
       image: `https://api.lorem.space/image/face?w=150&h=150`,
       likes: 0,
-      status: statuses[randomStatusIndex], // set status to a random value from the statuses array
-      species: species[randomSpeciesIndex], // set species to a random value from the species array
+      status,
+      species,
     };
   
     handleAddCharacter(newCharacter);
   
-    // clear the form
     setName("");
+    setStatus("Alive");
+    setSpecies("Human");
   }
-  
-
-  
 
   return (
     <div className="ui container">
@@ -48,7 +46,32 @@ function CharacterForm({ handleAddCharacter }) {
                 onChange={(e) => setName(e.target.value)}
               />
             </div>
-
+            <div className="field">
+              <label>Status</label>
+              <select
+                name="status"
+                className="input-text"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+              >
+                <option value="Alive">Alive</option>
+                <option value="Dead">Dead</option>
+                <option value="Unknown">Unknown</option>
+              </select>
+            </div>
+            <div className="field">
+              <label>Species</label>
+              <select
+                name="species"
+                className="input-text"
+                value={species}
+                onChange={(e) => setSpecies(e.target.value)}
+              >
+                <option value="Human">Human</option>
+                <option value="Alien">Alien</option>
+                <option value="Robot">Robot</option>
+              </select>
+            </div>
             <button type="submit" className="ui primary button">
               Add Character
             </button>
@@ -60,3 +83,6 @@ function CharacterForm({ handleAddCharacter }) {
 }
 
 export default CharacterForm;
+
+
+
